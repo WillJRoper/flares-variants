235961a3c8613f272b957a9d417b51d0c19b326d
wjr21 <wjr21@sussex.ac.uk>
2022-11-02 22:12:55 +0000

    Correcting label order

--- sfr_comp.py
+++ sfr_comp.py
@@ -121,9 +121,9 @@ def plot_ssfr_mass_vary(snap):
 
     # Define labels for each
     labels = ["AGNdT9", "REF", "SKIP",
+              "InstantFB", "$Z^0$", "$M_\dot=0$",
               "$f_{\mathrm{th, max}}=10$", "$f_{\mathrm{th, max}}=6$",
               "$f_{\mathrm{th, max}}=4$",
-              "InstantFB", "$Z^0$", "$M_\dot=0$",
               "$z_{\mathrm{reion}}=3.0$", "$z_{\mathrm{reion}}=7.5$",
               "$z_{\mathrm{reion}}=14.0$"]
 
