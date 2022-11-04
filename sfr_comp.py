d9b9ca2d41f85cbb5d899d2858654b0451107fd8
wjr21 <wjr21@sussex.ac.uk>
2022-11-04 17:06:07 +0000

    Fixed missing redshift

--- sfr_comp.py
+++ sfr_comp.py
@@ -578,7 +578,233 @@ def plot_stellarmet_mass_relation_evo_vary():
     # Save figure
     mkdir("plots/sfr/")
     fig.savefig("plots/sfr/ssfr_mass_evo.png",
-                bbox_inches="tight")    # Save figure
+                bbox_inches="tight")
+
+
+def plot_sfr_func_vary():
+
+    # Define binning
+    mass_bins = np.logspace(8.0, 13, 30)
+
+    # Define the path
+    ini_path = "/cosma/home/dp004/dc-rope1/FLARES/FLARES-1/<type>/data/"
+
+    # Define physics variations directories
+    types = ["flares_00", "FLARES_00_REF",
+             "FLARES_00_instantFB", "FLARES_00_noZSFthresh",
+             "flares_00_no_agn", "FLARES_00_highFBlim",
+             "FLARES_00_medFBlim", "FLARES_00_slightFBlim",
+             "flares_00_H_reion_z03",
+             "flares_00_H_reion_z075", "flares_00_H_reion_z14"]
+
+    # Define labels for each
+    labels = ["AGNdT9", "REF",
+              "InstantFB", "$Z^0$", "$M_\dot=0$",
+              "$f_{\mathrm{th, max}}=10$", "$f_{\mathrm{th, max}}=6$",
+              "$f_{\mathrm{th, max}}=4$",
+              "$z_{r, 0}$", "$z_{r, 7.5}$",
+              "$z_{r, 14}$"]
+
+    # Define linestyles
+    linestyles = ["-", "-", "--", "--", "--", "dotted", "dotted", "dotted",
+                  "dashdot", "dashdot", "dashdot"]
+
+    # Define snapshots
+    snaps = ["009_z006p000", "007_z008p000", "008_z007p000",
+             "009_z006p000", "010_z005p000"]
+
+    # Define plot dimensions
+    nrows = 1
+    ncols = len(snaps)
+
+    # Define norm
+    norm = LogNorm(vmin=1, vmax=10)
+
+    # Define hexbin extent
+    extent = [8, 11.5, -2, 1.5]
+
+    # Set up the plots
+    fig = plt.figure(figsize=(ncols * 3.5, nrows * 3.5))
+    gs = gridspec.GridSpec(nrows=nrows, ncols=ncols + 1,
+                           width_ratios=[20, ] * ncols + [1, ])
+    gs.update(wspace=0.0, hspace=0.0)
+    axes = []
+    cax = fig.add_subplot(gs[1:, -1])
+
+    for j in range(ncols):
+
+        # Define redshift
+        z = float(snaps[j].split("z")[-1].replace("p", "."))
+
+        # Create axis
+        ax = fig.add_subplot(gs[j])
+        ax.loglog()
+
+        # Include labels
+        if j == 0:
+            ax.set_ylabel(r"$\mathrm{sSFR} / [\mathrm{Gyr}^{-1}]$")
+        ax.set_xlabel(r"$M_\star / M_\odot$")
+
+        # Remove unnecessary ticks
+        if j > 0:
+            ax.tick_params("y", left=False, right=False,
+                           labelleft=False, labelright=False)
+
+        # Set axis limits
+        ax.set_ylim(10**extent[2], 10**extent[3])
+        ax.set_xlim(10**extent[0], 10**extent[1])
+
+        # Label axis
+        ax.text(0.95, 0.9, "$z=$%d" % z,
+                bbox=dict(boxstyle="round,pad=0.3", fc='w', ec="k", lw=1,
+                          alpha=0.8),
+                transform=ax.transAxes, horizontalalignment='right',
+                fontsize=8)
+
+        axes.append(ax)
+
+    for ax, snap in zip(axes, snaps):
+
+        # Define redshift
+        z = float(snap.split("z")[-1].replace("p", "."))
+
+        # What redshift was 100 Myrs ago?
+        z_100 = z_at_value(cosmo.age, cosmo.age(z) - (0.1 * u.Gyr),
+                           zmin=0, zmax=50)
+
+        for (ind, t), l in zip(enumerate(types), labels):
+
+            path = ini_path.replace("<type>", t)
+
+            print(path)
+
+            mass = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+                                       snap,
+                                       "Subhalo/ApertureMeasurements/Mass/030kpc",
+                                       noH=True, physicalUnits=True,
+                                       numThreads=8)[:, 4] * 10 ** 10
+            hmrs = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+                                       snap,
+                                       "Subhalo/HalfMassRad",
+                                       noH=True, physicalUnits=True,
+                                       numThreads=8)[:, 4] * 10 ** 3
+            cops = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+                                       snap,
+                                       "Subhalo/CentreOfPotential",
+                                       noH=True, physicalUnits=True,
+                                       numThreads=8) * 1000
+            grps = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+                                       snap,
+                                       "Subhalo/GroupNumber",
+                                       noH=True, physicalUnits=True,
+                                       numThreads=8)
+            subgrps = eagle_io.read_array("SUBFIND", path.replace("<type>", t),
+                                          snap,
+                                          "Subhalo/SubGroupNumber",
+                                          noH=True, physicalUnits=True,
+                                          numThreads=8)
+            birth_a = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+                                          snap,
+                                          "PartType4/StellarFormationTime",
+                                          noH=True, physicalUnits=True,
+                                          numThreads=8)
+            ini_mass = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+                                           snap,
+                                           "PartType4/InitialMass",
+                                           noH=True, physicalUnits=True,
+                                           numThreads=8) * 10 ** 10
+            coords = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+                                         snap,
+                                         "PartType4/Coordinates",
+                                         noH=True, physicalUnits=True,
+                                         numThreads=8) * 1000
+            part_grps = eagle_io.read_array("PARTDATA", path.replace("<type>", t),
+                                            snap,
+                                            "PartType4/GroupNumber",
+                                            noH=True, physicalUnits=True,
+                                            numThreads=8)
+            part_subgrps = eagle_io.read_array("PARTDATA",
+                                               path.replace("<type>", t),
+                                               snap,
+                                               "PartType4/SubGroupNumber",
+                                               noH=True, physicalUnits=True,
+                                               numThreads=8)
+
+            # Apply some cuts
+            mokinds = mass > 10**8
+            mass = mass[mokinds]
+            cops = cops[mokinds, :]
+            grps = grps[mokinds]
+            subgrps = subgrps[mokinds]
+            hmrs = hmrs[mokinds]
+
+            # Compute the birth redshift
+            birth_z = (1 / birth_a) - 1
+
+            # Get only particles born since z_100
+            okinds = birth_z < z_100
+            birth_z = birth_z[okinds]
+            ini_mass = ini_mass[okinds]
+            coords = coords[okinds, :]
+            part_grps = part_grps[okinds]
+            part_subgrps = part_subgrps[okinds]
+
+            # Set up array to store sfrs
+            ssfrs = []
+            ms = []
+            plt_hmrs = []
+
+            # Loop over galaxies
+            for igal in range(mass.size):
+
+                # Get galaxy data
+                m = mass[igal]
+                cop = cops[igal, :]
+                g = grps[igal]
+                sg = subgrps[igal]
+                hmr = hmrs[igal]
+
+                # Get this galaxies stars
+                sokinds = np.logical_and(part_grps == g, part_subgrps == sg)
+                this_coords = coords[sokinds, :] - cop
+                this_ini_mass = ini_mass[sokinds]
+
+                # Compute stellar radii
+                rs = np.sqrt(this_coords[:, 0] ** 2
+                             + this_coords[:, 1] ** 2
+                             + this_coords[:, 2] ** 2)
+
+                # Get only particles within the aperture
+                rokinds = rs < 30
+                this_ini_mass = this_ini_mass[rokinds]
+
+                # Compute and store ssfr
+                ssfrs.append(np.sum(this_ini_mass) / 0.1)
+                ms.append(m)
+                plt_hmrs.append(hmr)
+
+            # Convert to arrays
+            sfrs = np.array(ssfrs)
+            ms = np.array(ms)
+            ssfrs = sfrs / ms
+            plt_hmrs = np.array(plt_hmrs)
+            okinds = ssfrs > 0
+
+            plot_meidan_stat(ms[okinds], sfrs[okinds],
+                             np.ones(sfrs[okinds].size),
+                             ax, lab=l,
+                             color=None, bins=mass_bins,
+                             ls=linestyles[ind])
+
+    # Draw legend
+    axes[2].legend(loc='upper center',
+                   bbox_to_anchor=(0.5, -0.2),
+                   fancybox=True, ncol=3)
+
+    # Save figure
+    mkdir("plots/sfr/")
+    fig.savefig("plots/sfr/ssfr_mass_evo.png",
+                bbox_inches="tight")
 
 
 if __name__ == "__main__":
