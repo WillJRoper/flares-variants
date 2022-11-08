c83008f2eba5ee4590c9acb4ef6de04680cb91f5
wjr21 <wjr21@sussex.ac.uk>
2022-11-08 13:57:37 +0100

    Typo

--- gsmf_vary.py
+++ gsmf_vary.py
@@ -230,7 +230,7 @@ def plot_gsmf_evo_vary():
     # Draw legend
     axes[2].legend(loc='upper center',
                    bbox_to_anchor=(0.5, -0.2),
-                   fancybox=True, ncols=5)
+                   fancybox=True, ncol=5)
 
     # Save figure
     mkdir("plots/gsmf/")
